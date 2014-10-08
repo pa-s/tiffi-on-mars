073b6f0e50964f2ce0a5fb4b6300750afd1540eb
Sarah Pimentel <sarah.pimentel@gmai.com>
2014-10-08 22:48:52 +0100

    Implements Character#move

--- spec/characterSpec.js
+++ spec/characterSpec.js
@@ -1,4 +1,5 @@
 var Character = require('../src/character');
+var moveRules = require('../src/moveRules');
 
 describe("Character", function(){
   describe ("constructor", function(){
@@ -12,5 +13,94 @@ describe("Character", function(){
         cardinalDirection: cardinalDirection
       });
     });
+  });
+
+  describe("#move", function(){
+
+    beforeEach(function(){
+      this.character = new Character([5,1], 'N');
+    });
+
+    describe("when input is M", function(){
+      it("calls moveRules#coordinateForMovement", function(){
+        var spy = spyOn(moveRules, "coordinateForMovement"),
+          curCoordinate = this.character.currentPosition.coordinate,
+          curDirection = this.character.currentPosition.cardinalDirection;
+
+        this.character.move('M');
+
+        expect(spy).toHaveBeenCalledWith(curCoordinate, curDirection);
+      });
+
+      describe('When its cardinalDirection is N', function(){
+
+        beforeEach(function(){
+          this.character.currentPosition.cardinalDirection = 'N';
+        });
+
+        it("it increases its y position by 1", function(){
+          var curPos = this.character.currentPosition;
+
+          this.character.move('M');
+
+          expect(this.character.currentPosition.coordinate[1]).toBe(2);
+        });
+      });
+
+      describe('When its cardinalDirection is W', function(){
+
+        beforeEach(function(){
+          this.character.currentPosition.cardinalDirection = 'W';
+        });
+
+        it("it decreases its x position by 1", function(){
+          var curPos = this.character.currentPosition;
+
+          this.character.move('M');
+
+          expect(this.character.currentPosition.coordinate[0]).toBe(4);
+        });
+      });
+    });
+
+    describe("when input is L", function(){
+      it("calls moveRules#cardinalForDirection", function(){
+        var spy = spyOn(moveRules, "cardinalForDirection"),
+          curDirection = this.character.currentPosition.cardinalDirection,
+          direction = 'L';
+
+        this.character.move(direction);
+
+        expect(spy).toHaveBeenCalledWith(curDirection, direction);
+      });
+    });
+
+    describe("when input is R", function(){
+      it("calls moveRules#cardinalForDirection", function(){
+        var spy = spyOn(moveRules, "cardinalForDirection"),
+          curDirection = this.character.currentPosition.cardinalDirection,
+          direction = 'R';
+
+        this.character.move(direction);
+
+        expect(spy).toHaveBeenCalledWith(curDirection, direction);
+      });
+    });
+
+    describe('When its cardinalDirection is W', function(){
+
+      beforeEach(function(){
+        this.character.currentPosition.cardinalDirection = 'W';
+      });
+
+      it("it decreases its x position by 1", function(){
+        var curPos = this.character.currentPosition;
+
+        this.character.move('M');
+
+        expect(this.character.currentPosition.coordinate[0]).toBe(4);
+      });
+    });
+
   });
 });
--- src/character.js
+++ src/character.js
@@ -1,3 +1,5 @@
+var moveRules = require("./moveRules");
+
 
 function Character(coordinate, cardinalDirection) {
   this.currentPosition = {
@@ -5,5 +7,23 @@ function Character(coordinate, cardinalDirection) {
     cardinalDirection: cardinalDirection
   };
 };
+
+Character.prototype.move = function(movement){
+    switch (movement){
+      case 'M':
+        this.currentPosition.coordinate = moveRules.coordinateForMovement(
+          this.currentPosition.coordinate,
+          this.currentPosition.cardinalDirection
+        );
+        break;
+      case 'L':
+      case 'R':
+        this.currentPosition.cardinalDirection = moveRules.cardinalForDirection(
+          this.currentPosition.cardinalDirection,
+          movement
+        );
+        break;
+    }
+}
 
 module.exports = Character;
